79efad019d008517587c9de49114fe4b743fa4e6
Aisherr <aishermohamed123@gmail.com>
2023-06-12 10:23:10 +0300

    updated notes on Variables and Arrays lesson

--- 02_variables.php
+++ 02_variables.php
@@ -3,22 +3,43 @@
 /* ----- Variables & Data Types ----- */
 
 /* --------- PHP Data Types --------- */
-/*
+/*data types are a special characters and symbols used to store data
 - String - A string is a series of characters surrounded by quotes
 - Integer - Whole numbers
 - Float - Decimal numbers
 - Boolean - true or false
 - Array - An array is a special variable, which can hold more than one value
-- Object - A class
 - NULL - Empty variable
 - Resource - A special variable that holds a resource
 */
 
 /* --------- Variable Rules --------- */
 /*
+/*A Variable is a named memory location
 - Variables must be prefixed with $
 - Variables must start with a letter or the underscore character
 - variables can't start with a number
 - Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 - Variables are case-sensitive ($name and $NAME are two different variables)
 */
+
+
+$name="Aisha";
+
+$age=20;
+
+$favfood="pizza";
+
+$gender='female';
+
+
+echo " Hello My name is $name am $age yrs old my favourite food is $favfood and my gender is $gender<br>"; 
+
+$cars=8;
+$momcars=2;
+$brocars=2;
+$dadcar=1;
+$remainingcars= $cars-($momcars+$brocars+$dadcar);
+echo "$remainingcars";
+
+
--- 03_arrays.php
+++ 03_arrays.php
@@ -1,6 +1,58 @@
 <?php
 /* ----------- Arrrays ----------- */
 
+
 /*
+  Arrays are collection of items/data of the same datatype stored at contegious memory location.
   Arrays hold multiple values. Each value in an array is called an "element"
 */
+//Examples of an array
+//Types of Arrays
+   //Index Arrays
+     //simple Array of numbers
+         $numbers=[1,2,3,4,5,6,7,8,9,];
+    //simple Array of strings
+         $colors=["yellow","white","red","green","blue","purple"];
+         $colorss="colorless";
+//echo $colorss;
+//echo $colors[4];
+
+//var_dump($colors);
+//print_r($numbers);
+      //Associative Arrays-allow us to use named keys to identify values
+
+      $rangi=[
+        'red' => '#f00' ,
+        'green' => '#0f0',
+        'blue' => '00f'
+      ];
+      //echo $rangi['red'];
+      //var_dump($rangi);
+
+      //multi-dimensional Arrays-often used to store data in a table form
+
+      $people = [
+        $person1=[
+          'firstname' => 'Aisha',
+          'lastname' => 'Mohamed',
+          'email' => 'aisher123@gmail.com'
+        ],
+        $person2=[
+          'firstname' => 'Yasmin',
+          'lastname' => 'Ahmed',
+          'email' => 'yasmin345@gmail.com'
+        ],
+        $person3=[
+          'firstname' => 'jacqueline',
+          'lastname' => 'njoki',
+          'email' => 'njoki832@gmail.com'
+        ]
+
+        ];
+        //var_dump($people);
+        echo $people[1]['email'];
+
+
+     
+
+
